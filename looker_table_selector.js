8cf236fcee0517b6b0e193ca10525d31b0e6dcd1
kendygt <kendygt@gmail.com>
2026-02-09 09:41:38 -0300

    Delete looker_table_selector.js

--- looker_table_selector.js
+++
@@ -1,7 +0,0 @@
-// código inicial mínimo que o Looker reconhece
-function drawViz(vizData) {
-  console.log(vizData);
-  document.body.innerHTML = "<h3>Visual pronto!</h3>";
-}
-
-dscc.subscribeToData(drawViz, {});
